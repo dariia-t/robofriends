6cb716fa3ea7a381c1b4de3bf10ebfe4d8b209f3
dat225 <dat225@lehigh.edu>
2023-05-26 22:05:34 -0400

    header font added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import CardList from "./CardList";
 import {robots} from './robots';
 import SearchBox from './SearchBox';
-
+import './App.css'
 class App extends Component {
     constructor() {
         super()
@@ -17,12 +17,12 @@ class App extends Component {
     }
 
     render(){
-        const filteredRobots = this.state.robots.filter(robot =>{
-            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase);
+        const filteredRobots = this.state.robots.filter(robots =>{
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
         return (
             <div className="tc"> 
-                <h1>RoboFriends</h1>
+                <h1 className='f1'>RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
                 <CardList robots={filteredRobots}/>
             </div>
